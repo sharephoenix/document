ad8a939dcf04cd92a325d6a64974eb16c5522d93
alexluan <326083325@qq.com>
2019-06-17 23:02:27 +0800

    fix

--- docs/golang/weexdemogo/main/main.go
+++ docs/golang/weexdemogo/main/main.go
@@ -4,9 +4,14 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"weexdemogo/logic"
 	_ "weexdemogo/datastruct"
+	"fmt"
 )
 
 func main() {
 	logic.Start()
 }
 
+
+func test() {
+	fmt.Println("12312312312")
+}
